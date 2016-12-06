599bf93d70733eb7211136b5cd691693e801caed
彭启伟 <pengqiwei@pengqiweideMacBook-Pro.local>
2016-12-06 18:40:22 +0800

    12121

--- MarkWrite/ShellFrame/SettingControl/SettingViewController.m
+++ MarkWrite/ShellFrame/SettingControl/SettingViewController.m
@@ -9,8 +9,10 @@
 #import "SettingViewController.h"
 #import "SettingTableViewCell.h"
 
-@interface SettingViewController ()<UITableViewDelegate,UITableViewDataSource>
+@interface SettingViewController ()<UITableViewDelegate,UITableViewDataSource,CellSwitchClickDelegate>
 @property (nonatomic, strong) UITableView *tableView;
+@property (nonatomic, assign) NSInteger sectionNum;
+@property (nonatomic, strong) NSArray *sectionTitle;
 
 @end
 
@@ -23,54 +25,38 @@
 #pragma mark - UI
 - (void)initUserInterface {
     self.title = @"设置";
+    _sectionNum = 1;
+    _sectionTitle = @[@"密码"];
     self.view.backgroundColor = [UIColor whiteColor];
     [self.view addSubview:self.tableView];
 }
 #pragma mark - UITableViewDataSource
 //行数
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//    switch (section) {
-//        case 0: {
-//            
-//        }
-//            break;
-//        case 1: {
-//            
-//        }
-//            break;
-//        case 2: {
-//            
-//        }
-//            break;
-//        case 3: {
-//            
-//        }
-//            break;
-//            
-//        default:
-//            break;
-//    }
     if (section == 1) {
         return 2;
+    }else if (section == 2){
+        return _sectionNum;
     }else {
         return 1;
     }
 }
 //区数
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 4;
+    return 5;
 }
 //配置cell
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *cellIdentifer = @"setting";
     SettingTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifer];
     if (!cell) {
-        cell = [[SettingTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifer];
+        cell = [[SettingTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifer section:indexPath.section];
     }
+    cell.delegate = self;
     switch (indexPath.section) {
         case 0: {
-            cell.textLabel.text = @"键盘辅助";
-            cell.switchView.hidden = NO;
+            cell.textLabel.text = @"辅助键盘";
+            cell.switchButton.hidden = NO;
         }
             break;
         case 1: {
@@ -80,13 +66,39 @@
         }
             break;
         case 2: {
+            cell.switchButton.hidden = NO;
+            cell.accessoryType = UITableViewCellStyleDefault;
+            cell.textLabel.text = _sectionTitle[indexPath.row];
+            if (_sectionTitle.count == 3) {
+                switch (indexPath.row) {
+                    case 0: {
+                        cell.switchButton.hidden = NO;
+                    }
+                        break;
+                    case 1: {
+                        cell.switchButton.hidden = YES;
+                    }
+                        break;
+                    case 2: {
+                        cell.switchButton.hidden = NO;
+                    }
+                        break;
+                        
+                    default:
+                        break;
+                }
+            }
+        }
+            break;
+        case 3: {
             cell.textLabel.text = @"关于";
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
         }
             break;
-        case 3: {
+        case 4: {
             cell.textLabel.text = @"打赏";
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+            cell.switchButton.hidden = YES;
         }
             break;
             
@@ -127,17 +139,41 @@
             }
         }
             break;
-        case 2: {
+        case 3: {
             AboutUsViewController *aboutVC = [[AboutUsViewController alloc] init];
             [self.navigationController pushViewController:aboutVC animated:YES];
         }
             break;
-        case 3: {
+        case 4: {
             [QWPTools showMessageWithTitle:@"给口狗粮" content:@"MartWrite是一款很实用的App，如果您觉得这款软件还适合您的话请给口狗粮吧，谢谢！" buttonTitles:@[@"取消",@"狗粮6元"] clickedHandle:^(NSInteger index) {
                 
             } compeletedHandle:^{
                 
             }];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+#pragma mark - CellSwitchClickDelegate
+- (void)cellSwitchClick:(UISwitch *)sender {
+    switch (sender.tag) {
+        case 0: {
+            
+        }
+            break;
+        case 2: {
+            if (sender.isOn) {
+                _sectionNum = 3;
+                _sectionTitle = @[@"touchID",@"pass",@"mima"];
+                [self.tableView reloadData];
+            }else {
+                _sectionNum = 1;
+                _sectionTitle = @[@"密码"];
+                [self.tableView reloadData];
+            }
         }
             break;
             
