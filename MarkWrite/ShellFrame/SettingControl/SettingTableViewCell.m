f5f5cfa73cc9b739bfbc4a4f5537240b2b140a57
彭启伟 <pengqiwei@pengqiweideMacBook-Pro.local>
2016-12-08 11:14:58 +0800

    12-8-11-14

--- MarkWrite/ShellFrame/SettingControl/SettingTableViewCell.m
+++ MarkWrite/ShellFrame/SettingControl/SettingTableViewCell.m
@@ -22,8 +22,6 @@
 - (void)setupUIWithSection:(NSInteger)section withRow:(NSInteger)row {
     _switchButton = [[UISwitch alloc] initWithFrame:AAdaptionRect(600, 15, 100, 80)];
     _switchButton.hidden = YES;
-    NSString *string = [NSString stringWithFormat:@"%ld.%ld",section,row];
-    NSLog(@"%f",string.floatValue);
     _switchButton.tag = section + row;
     [_switchButton setOn:NO];
     [_switchButton addTarget:self action:@selector(switchButtonAction:) forControlEvents:UIControlEventValueChanged];
